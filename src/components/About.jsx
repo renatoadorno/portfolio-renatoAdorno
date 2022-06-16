5a45c1a7cb635753556c4bb42dc8959b9afdeb96
renato <renatowillian.2000@gmail.com>
2022-06-16 03:35:02 -0300

    feat: atualizando link de download do cv

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -42,7 +42,7 @@ export default function About() {
             </div>
 
             <div className="about__buttons">
-              <a download href="https://doc-0k-ao-docs.googleusercontent.com/docs/securesc/4l0e6iuhdkjor1bv5uglj1bqp3ugeuv2/9sgapcsmvqd3b3n62ro90ditmncajrru/1655360625000/06777320144468424285/06777320144468424285/1uMyWUP8rn1BwvrkIqUCiCMmnNNQia91U?e=download&ax=ACxEAsYkGKA-rL3qfygiEbEbJVZGsRddPIQ72UZ8XM_YtJoJ04_yS7NCf6cwVrjJR6gjzlDCZptFBGLp72WBhzhQtDzLX76wOxD4c04Ulq8RXWgWnGEtuCXjzh-M3G0n3xKnyChvlEgaUJG1YOqNi5nOTX-9xxoa9LmGDGz2xIF43dis3IxgL62OpwXa6HNJPbqLZQ61A2CR7lDonaaN_4AMtBvQQVzSr7Rp-M7yXr7bRBKJkj-9iaSNyC70HZsQj0OuxYSvLK5YjDzyxm91sDfwv7QetsX4VG8eSNu6vNtaQrt23uBVQwuxrSFvBArkXdVARV5ZBf8JFxSaOoEco0zJjAnk1AYCGGVtxRpb4FbtPfSmlYoWJrETpgtN-Zd9tLyR_Nh5FR_Uu96ubwLnEXFdDEKwqewsDjFdM_o0z2QiIM_3wDKk0tWfDHZPwtssLbQmLI1AYo6_9MXj-ympdXcZmU6uaO7sqYQFQw8b3Ee4gyCspBT9DYU6KDZUMP7nRlOms_C2vxbek_trQWcemtZgyHnVrVct7H6wHqcl9cTRFIp3VbIF7YDuIdUgmYv-OQmFaDZPODQI71qOaKDiHAjeMId5jEeKYfbwzRazlAlbFDKqC5IDi8RDMBWoEUErNz_NRH_BR_Gvj3vbxMZGVuU-nuQ9qluoG7KD7pk3GBHzfLXv3LT3nUlAhDeGoB8VMjZorQXw0a5DSV8T2imdSkwTTvrPQh-59NfQo34HvivSwsjlIrIU65M&authuser=1&nonce=jcjdt0cs4me20&user=06777320144468424285&hash=8vq2vhbto9qaakolu5i3q5avovue1osq" className="button button--flex">
+              <a href="https://drive.google.com/file/d/1uMyWUP8rn1BwvrkIqUCiCMmnNNQia91U/view?usp=sharing" className="button button--flex" target="_blank" rel="noreferrer">
                 Download CV <FiDownload className="button__icon" />
               </a>
             </div>
